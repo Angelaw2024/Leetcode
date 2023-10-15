c743656d2adf8bf17078b852b7ed6c506e868bba
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-15 16:49:06 -0700

    Time: 3 ms (47.08%), Space: 42 MB (89.43%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -1,6 +1,8 @@
 class Solution {
     /**
     那就是字符串的每个字母和首字符的相对距离都是相等的，比如 abc 和 efg 互为偏移，对于 abc 来说，b和a的距离是1，c和a的距离是2，对于 efg 来说，f和e的距离是1，g和e的距离是2。再来看一个例子，az 和 yx，z和a的距离是 25，x和y的距离也是 25 (直接相减是 -1，这就是要加 26 然后取余的原因)，那么这样的话，所有互为偏移的字符串都有个 unique 的距离差，根据这个来建立映射就可以很好的进行单词分组了
+    生成key -> hashMap 分类 -> 装换为结果列表
+    如果我们假设输入的字符串数量为 N，每个字符串的平均长度为 L，那么构建键值的时间复杂度为 O(N * L)
      */
     public List<List<String>> groupStrings(String[] strings) {
         HashMap<String, List<String>> map = new HashMap<>();
