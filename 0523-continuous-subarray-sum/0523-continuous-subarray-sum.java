6433b93a2e551789b625663c7ddfdc9504191786
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-15 11:54:05 -0700

    Time: 26 ms (15.83%), Space: 60.8 MB (19.09%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,33 +1,33 @@
-// class Solution {
-//     /**
-//     This helped me visualize the ans. In the example:
-// [23,2,6,4,7] k =12
-
-// (23)%12 = 11
-// (23+2)%12=1
-// (23+2+6)%12 = 7
-// (23+2+6+4)%12=11 -> seen before, so return true because:
-
-// (23+2+6+4) - (23) = 12 -> 12 % 12 = 0
-//  */
-//     public boolean checkSubarraySum(int[] nums, int k) {
-//         if (nums.length <= 1) return false;
-//         Map<Integer, Integer> map = new HashMap<>();
-//         map.put(0, 0);
-//         int sum = 0;
-//         for (int i = 0; i < nums.length; i++) {
-//             sum += nums[i];
-//             if (!map.containsKey(sum % k)) {
-//                 map.put(sum % k, i + 1);
-//             } else if (map.get(sum % k) < i) {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// }
-
 class Solution {
+    /**
+    This helped me visualize the ans. In the example:
+[23,2,6,4,7] k =12
+
+(23)%12 = 11
+(23+2)%12=1
+(23+2+6)%12 = 7
+(23+2+6+4)%12=11 -> seen before, so return true because:
+
+(23+2+6+4) - (23) = 12 -> 12 % 12 = 0
+ */
+    public boolean checkSubarraySum(int[] nums, int k) {
+        if (nums.length <= 1) return false;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (!map.containsKey(sum % k)) {
+                map.put(sum % k, i);
+            } else if (i - map.get(sum % k) > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class Solution1 {
     public boolean checkSubarraySum(int[] nums, int k) {
         if (nums.length < 2) return false;
         int sum = 0;
