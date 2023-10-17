6eca92186dc9f1e34f06340f471703610f384ac4
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-17 09:58:41 -0700

    Time: 27 ms (43.08%), Space: 41.5 MB (90.96%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,19 +19,36 @@ class Node {
 */
 
 class Solution {
-    private HashMap <Node, Node> visited = new HashMap <> ();
+    Node[] visited = new Node[101];
     public Node cloneGraph(Node node) {
         if (node == null) return node;
 
-        if (visited.containsKey(node)) {
-            return visited.get(node);
+        if (visited[node.val] != null) {
+            return visited[node.val];
         }
 
         Node copy = new Node(node.val, new ArrayList<>());
-        visited.put(node, copy);
+        visited[copy.val] = copy;
         for (Node n: node.neighbors) {
             copy.neighbors.add(cloneGraph(n));
         }
         return copy;
     }
 }
+
+// class Solution1 {
+//     public Node cloneGraph(Node node) {
+//         if (node == null) return node;
+
+//         if (visited.containsKey(node)) {
+//             return visited.get(node);
+//         }
+
+//         Node copy = new Node(node.val, new ArrayList<>());
+//         visited.put(node, copy);
+//         for (Node n: node.neighbors) {
+//             copy.neighbors.add(cloneGraph(n));
+//         }
+//         return copy;
+//     }
+// }
