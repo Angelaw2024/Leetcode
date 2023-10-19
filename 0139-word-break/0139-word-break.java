c1cf0625c2f407550a9f7c080a21db0d8f51f2bd
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-18 21:00:25 -0700

    Time: 0 ms (100%), Space: 40.1 MB (99.22%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,15 +1,38 @@
 class Solution {
+    // public boolean wordBreak(String s, List<String> wordDict) {
+    //     Set<String> set = new HashSet<>(wordDict);
+    //     boolean[] valid = new boolean[s.length() + 1];
+    //     valid[0] = true;
+    //     for (int i = 1; i <= s.length(); i++) {
+    //         for (int j = i - 1; j >= 0; j--) {
+    //             if (valid[j] && set.contains(s.substring(j, i))) {
+    //                 valid[i] = true;
+    //             }
+    //         }
+    //     }
+    //     return valid[s.length()];
+    // }
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> set = new HashSet<>(wordDict);
-        boolean[] valid = new boolean[s.length() + 1];
-        valid[0] = true;
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (valid[j] && set.contains(s.substring(j, i))) {
-                    valid[i] = true;
+        Boolean[] dp = new Boolean[s.length()+1];
+        return wordBreak(s, dp, wordDict, 0);
+    }
+
+    private boolean wordBreak(String s, Boolean[] dp, List<String> wordDict, int k){
+        if(k==s.length()){
+            return true;
+        }
+        if(dp[k]!=null){
+            return dp[k];
+        }
+        for(int i=0;i<wordDict.size();i++){
+            if(s.startsWith(wordDict.get(i), k)){
+                if(wordBreak(s, dp, wordDict, k+wordDict.get(i).length())){
+                    dp[k] = true;
+                    return true;
                 }
             }
         }
-        return valid[s.length()];
+        dp[k] = false;
+        return false;
     }
 }
