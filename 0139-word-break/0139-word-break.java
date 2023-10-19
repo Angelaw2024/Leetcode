bec3257e4838a38a23db7f2d3bd596787eb62e7e
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-18 21:03:02 -0700

    Time: 7 ms (49.96%), Space: 43.6 MB (25.4%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,38 +1,40 @@
 class Solution {
-    // public boolean wordBreak(String s, List<String> wordDict) {
-    //     Set<String> set = new HashSet<>(wordDict);
-    //     boolean[] valid = new boolean[s.length() + 1];
-    //     valid[0] = true;
-    //     for (int i = 1; i <= s.length(); i++) {
-    //         for (int j = i - 1; j >= 0; j--) {
-    //             if (valid[j] && set.contains(s.substring(j, i))) {
-    //                 valid[i] = true;
-    //             }
-    //         }
-    //     }
-    //     return valid[s.length()];
-    // }
     public boolean wordBreak(String s, List<String> wordDict) {
-        Boolean[] dp = new Boolean[s.length()+1];
-        return wordBreak(s, dp, wordDict, 0);
-    }
-
-    private boolean wordBreak(String s, Boolean[] dp, List<String> wordDict, int k){
-        if(k==s.length()){
-            return true;
-        }
-        if(dp[k]!=null){
-            return dp[k];
-        }
-        for(int i=0;i<wordDict.size();i++){
-            if(s.startsWith(wordDict.get(i), k)){
-                if(wordBreak(s, dp, wordDict, k+wordDict.get(i).length())){
-                    dp[k] = true;
-                    return true;
+        Set<String> set = new HashSet<>(wordDict);
+        boolean[] valid = new boolean[s.length() + 1];
+        valid[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (valid[j] && set.contains(s.substring(j, i))) {
+                    valid[i] = true;
+                    break;
                 }
             }
         }
-        dp[k] = false;
-        return false;
+        return valid[s.length()];
     }
+
+    // public boolean wordBreak(String s, List<String> wordDict) {
+    //     Boolean[] dp = new Boolean[s.length()+1];
+    //     return wordBreak(s, dp, wordDict, 0);
+    // }
+
+    // private boolean wordBreak(String s, Boolean[] dp, List<String> wordDict, int k){
+    //     if(k==s.length()){
+    //         return true;
+    //     }
+    //     if(dp[k]!=null){
+    //         return dp[k];
+    //     }
+    //     for(int i=0;i<wordDict.size();i++){
+    //         if(s.startsWith(wordDict.get(i), k)){
+    //             if(wordBreak(s, dp, wordDict, k+wordDict.get(i).length())){
+    //                 dp[k] = true;
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     dp[k] = false;
+    //     return false;
+    // }
 }
