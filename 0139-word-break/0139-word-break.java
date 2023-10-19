992c6556272c459e5a96cbc5d4cfd1674d56b046
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-18 21:09:07 -0700

    Time: 4 ms (71.3%), Space: 41.5 MB (72.53%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -14,38 +14,83 @@ class Solution {
     //     return valid[s.length()];
     // }
 
-    private String s;
-    private List<String> wordDict;
-    private int[] memo;
+    // private String s;
+    // private List<String> wordDict;
+    // private int[] memo;
     
-    private boolean dp(int i) {
-        if (i < 0) return true;
+    // private boolean dp(int i) {
+    //     if (i < 0) return true;
         
-        if (memo[i] != -1) {
-            return memo[i] == 1;
-        }
+    //     if (memo[i] != -1) {
+    //         return memo[i] == 1;
+    //     }
         
-        for (String word: wordDict) {
-            // Handle out of bounds case
-            if (i - word.length() + 1 < 0) {
-                continue;
-            }
+    //     for (String word: wordDict) {
+    //         // Handle out of bounds case
+    //         if (i - word.length() + 1 < 0) {
+    //             continue;
+    //         }
             
-            if (s.substring(i - word.length() + 1, i + 1).equals(word) && dp(i - word.length())) {
-                memo[i] = 1;
-                return true;
-            }
-        }
+    //         if (s.substring(i - word.length() + 1, i + 1).equals(word) && dp(i - word.length())) {
+    //             memo[i] = 1;
+    //             return true;
+    //         }
+    //     }
         
-        memo[i] = 0;
-        return false;
+    //     memo[i] = 0;
+    //     return false;
+    // }
+    
+    // public boolean wordBreak(String s, List<String> wordDict) {
+    //     this.s = s;
+    //     this.wordDict = wordDict;
+    //     this.memo = new int[s.length()];
+    //     Arrays.fill(this.memo, -1);
+    //     return dp(s.length() - 1);
+    // }
+
+    class TrieNode {
+        boolean isWord;
+        Map<Character, TrieNode> children;
+        TrieNode() {
+            this.children = new HashMap<>();
+        }
     }
     
     public boolean wordBreak(String s, List<String> wordDict) {
-        this.s = s;
-        this.wordDict = wordDict;
-        this.memo = new int[s.length()];
-        Arrays.fill(this.memo, -1);
-        return dp(s.length() - 1);
+        TrieNode root = new TrieNode();
+        for (String word: wordDict) {
+            TrieNode curr = root;
+            for (char c: word.toCharArray()) {
+                if (!curr.children.containsKey(c)) {
+                    curr.children.put(c, new TrieNode());
+                }
+
+                curr = curr.children.get(c);
+            }
+            
+            curr.isWord = true;
+        }
+        
+        boolean[] dp = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0 || dp[i - 1]) {
+                TrieNode curr = root;
+                for (int j = i; j < s.length(); j++) {
+                    char c = s.charAt(j);
+                    if (!curr.children.containsKey(c)) {
+                        // No words exist
+                        break;
+                    }
+                    
+                    curr = curr.children.get(c);
+                    if (curr.isWord) {
+                        dp[j] = true;
+                    }
+                }
+            }
+        }
+        
+        return dp[s.length() - 1];
     }
 }
