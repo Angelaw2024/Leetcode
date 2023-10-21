06ad4a9a03893a818891109f6cc8eb888f42ffb0
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-20 22:36:27 -0700

    Time: 31 ms (63.94%), Space: 85.5 MB (5.2%) - LeetHub

---
+++ 1539-diagonal-traverse-ii/1539-diagonal-traverse-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i < nums.size();  i++) {
+            for (int j = 0; j < nums.get(i).size(); j++) {
+                count++;
+                int idx = i + j;
+                map.computeIfAbsent(idx, k -> new ArrayList<>()).add(0, nums.get(i).get(j));
+            }
+        }
+        int[] res = new int[count];
+        int n = 0;
+        for (int i = 0; i < map.keySet().size(); i++) {
+            List<Integer> list = map.get(i);
+            for (int j = 0; j < list.size(); j++) {
+                res[n++] = list.get(j);
+            }
+        }
+        return res;
+    }
+}
