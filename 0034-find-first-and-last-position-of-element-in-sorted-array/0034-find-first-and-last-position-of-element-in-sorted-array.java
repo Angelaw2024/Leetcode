16f12bd474af9893521a5ff07e87e732dc6922e1
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-21 11:27:27 -0700

    Time: 1 ms (23.42%), Space: 44.5 MB (34.78%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,50 +1,61 @@
-// class Solution {
-//     public int[] searchRange(int[] nums, int target) {
-//         int[] res = helper(nums, target, 0, nums.length - 1);
-//         return res;
-//     }
-//     public int[] helper(int[] nums, int target, int l, int r) {
-//         if (l > r) return new int[]{-1, -1};
-//         if (l == r) {
-//             if (nums[l] == target) return new int[]{l, l};
-//             return new int[]{-1, -1};
-//         }
-//         int mid = l + (l - r) / 2;
-//         int[] left = helper(nums, target, l, mid);
-//         int[] right = helper(nums, target, mid + 1, r);
-//         int[] cur = new int[2];
-//         cur[0] = Math.min(left[0], right[0]);
-//         cur[1] = Math.max(left[1], right[1]);
-//         return cur;
-
-//     }
-// }
-
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) return new int[]{-1, -1};
-
-        int first = findFirst(nums, target);
-        int last = findFirst(nums, target + 1);
-        int[] res = new int[]{-1, -1};
-        if (nums[first] != target) return res;
-        res[0] = first;
-        res[1] = nums[last] == target ? last : last - 1;
-        return res;
+        int[] toReturn = divide_and_conquer(nums, target, 0, nums.length - 1);
+        return toReturn[0] < Integer.MAX_VALUE ? toReturn : new int[] {-1, -1};
     }
-
-    private int findFirst(int[] nums, int target) {
-        int n = nums.length;
-        int left = 0;
-        int right = n - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+    
+    public int[] divide_and_conquer(int[] nums, int target, int low, int high) {
+        int[] ret = {Integer.MAX_VALUE, Integer.MIN_VALUE};
+        
+        if(low > high) return ret;
+        
+        if(low == high && nums[low] == target) {
+            ret[0] = low;
+            ret[1] = high;
+            return ret;
         }
-        return left;
+        // might need to go
+        else if(low == high) {
+            return ret;
+        }
+        
+        else {
+            int mid = low + (high - low)/2;
+            int[] left = divide_and_conquer(nums, target, low, mid);
+            int[] right = divide_and_conquer(nums, target, mid+1, high);
+            
+            ret[0] = Math.min(left[0], right[0]);
+            ret[1] = Math.max(left[1], right[1]);
+        }
+        return ret;
     }
 }
+
+// class Solution1 {
+//     public int[] searchRange(int[] nums, int target) {
+//         if (nums.length == 0) return new int[]{-1, -1};
+
+//         int first = findFirst(nums, target);
+//         int last = findFirst(nums, target + 1);
+//         int[] res = new int[]{-1, -1};
+//         if (nums[first] != target) return res;
+//         res[0] = first;
+//         res[1] = nums[last] == target ? last : last - 1;
+//         return res;
+//     }
+
+//     private int findFirst(int[] nums, int target) {
+//         int n = nums.length;
+//         int left = 0;
+//         int right = n - 1;
+//         while (left < right) {
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] < target) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid;
+//             }
+//         }
+//         return left;
+//     }
+// }
