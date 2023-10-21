90b2c20cee33123276cb11af67578b0bbf63b471
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-20 22:51:05 -0700

    Time: 25 ms (81.78%), Space: 68.5 MB (72.49%) - LeetHub

--- 1539-diagonal-traverse-ii/1539-diagonal-traverse-ii.java
+++ 1539-diagonal-traverse-ii/1539-diagonal-traverse-ii.java
@@ -1,22 +1,55 @@
 class Solution {
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        int count = 0;
-        for (int i = 0; i < nums.size();  i++) {
-            for (int j = 0; j < nums.get(i).size(); j++) {
-                count++;
-                int idx = i + j;
-                map.computeIfAbsent(idx, k -> new ArrayList<>()).add(nums.get(i).get(j));
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        queue.offer(new Pair(0, 0));
+        List<Integer> ans = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                Pair<Integer, Integer> p = queue.poll();
+                int row = p.getKey();
+                int col = p.getValue();
+                ans.add(nums.get(row).get(col));
+                if (col == 0 && row + 1 < nums.size()) {
+                    queue.offer(new Pair(row + 1, col));
+                }
+                if (col + 1 < nums.get(row).size()) {
+                    queue.offer(new Pair(row, col + 1));
+                }
             }
         }
-        int[] res = new int[count];
-        int n = 0;
-        for (int i = 0; i < map.keySet().size(); i++) {
-            List<Integer> list = map.get(i);
-            for (int j = list.size() - 1; j >= 0; j--) {
-                res[n++] = list.get(j);
-            }
+
+        int[] result = new int[ans.size()];
+        int i = 0;
+        for (int num : ans) {
+            result[i] = num;
+            i++;
         }
-        return res;
+        
+        return result;
     }
 }
+
+// class Solution1 {
+//     public int[] findDiagonalOrder(List<List<Integer>> nums) {
+//         Map<Integer, List<Integer>> map = new HashMap<>();
+//         int count = 0;
+//         for (int i = 0; i < nums.size();  i++) {
+//             for (int j = 0; j < nums.get(i).size(); j++) {
+//                 count++;
+//                 int idx = i + j;
+//                 map.computeIfAbsent(idx, k -> new ArrayList<>()).add(nums.get(i).get(j));
+//             }
+//         }
+//         int[] res = new int[count];
+//         int n = 0;
+//         for (int i = 0; i < map.keySet().size(); i++) {
+//             List<Integer> list = map.get(i);
+//             for (int j = list.size() - 1; j >= 0; j--) {
+//                 res[n++] = list.get(j);
+//             }
+//         }
+//         return res;
+//     }
+// }
