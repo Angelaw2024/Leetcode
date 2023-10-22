dfa73e20ae7ac91759db5bdea7fc30ba3e9f23fb
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-21 17:33:32 -0700

    Time: 8 ms (41.54%), Space: 40.5 MB (50.15%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String countAndSay(int n) {
+        if (n <= 0) return "";
+        String res = "1";
+        while(--n> 0) {
+            StringBuilder cur = new StringBuilder();
+            for (int i = 0; i < res.length(); i++) {
+                int count = 1;
+                while (i + 1 < res.length() && res.charAt(i) == res.charAt(i + 1)) {
+                    count++;
+                    i++;
+                }
+                cur.append(count);
+                cur.append(res.charAt(i));
+            }
+            res = cur.toString();
+        }
+        return res;
+    }
+}
