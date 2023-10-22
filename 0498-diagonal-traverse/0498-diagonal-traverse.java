d59c7f8973e5a5c56a71de1da39b4e09d16514bf
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-21 17:52:00 -0700

    Time: 2 ms (98.69%), Space: 46 MB (78.11%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,35 +1,37 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        if (mat == null || mat.length == 0) {
-            return new int[0];
-        }
-
-        int n = mat.length;
-        int m = mat[0].length;
-        int row = 0, column = 0;
-        int direction = 1;
-        int[] result = new int[n * m];
-        int r = 0;
-
-        while (row < n && column < m) {
-            result[r++] = mat[row][column];
-            int new_row = row + (direction == 1 ? -1 : 1);
-            int new_column = column + (direction == 1 ? 1 : -1);
-            if (new_row < 0 || new_row == n || new_column < 0 || new_column == m) {    
-                if (direction == 1) {
-                    row += (column == m - 1 ? 1 : 0) ;
-                    column += (column < m - 1 ? 1 : 0);  
+        int rowMax = mat.length;
+        int colMax = mat[0].length;
+        int nItems = rowMax * colMax;
+        int[] result = new int[nItems];        
+        
+        rowMax--; colMax--;
+        boolean up = true;
+        
+        for(int i=0, row=0, col=0; i < nItems; i++) {
+            result[i] = mat[row][col];
+            if(up){
+                if(col == colMax){ // Check this condition first before the other else-if condition otherwise you'll get ArrayOutOfBoundsException
+                    row++;
+                    up=false;
+                } else if (row == 0){
+                    col++;
+                    up=false;
                 } else {
-                    column += (row == n - 1 ? 1 : 0);
-                    row += (row < n - 1 ? 1 : 0);
+                    row--; col++;
                 }
-                direction = 1 - direction;         
             } else {
-                
-                row = new_row;
-                column = new_column;
-            }
+                if(row == rowMax){
+                    col++;
+                    up = true;
+                } else if(col == 0) {
+                    row++;
+                    up = true;
+                } else {
+                    row++; col--;
+                }
+            }      
         }
-        return result;      
+        return result;
     }
 }
