e50a8b78b721d5750f203d189ea241baa4a6bcd3
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-23 23:40:45 -0700

    Time: 1 ms (34.89%), Space: 39 MB (64.51%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int nextGreaterElement(int n) {
+        StringBuilder numStr = new StringBuilder(Integer.toString(n));
+        int len = numStr.length();
+        
+        // 1. Find the first decreasing digit from the right
+        int i = len - 2;
+        while (i >= 0 && numStr.charAt(i) >= numStr.charAt(i + 1)) {
+            i--;
+        }
+        
+        // If no such digit is found, return -1
+        if (i == -1) return -1;
+
+        // 2. Swap it with the smallest digit found to its right that is greater than it
+        int j = len - 1;
+        while (numStr.charAt(j) <= numStr.charAt(i)) {
+            j--;
+        }
+        char tmp = numStr.charAt(i);
+        numStr.setCharAt(i, numStr.charAt(j));
+        numStr.setCharAt(j, tmp);
+System.out.print(numStr.toString());
+        // 3. Reverse the substring to the right of the swapped digit
+        numStr.replace(i + 1, len, new StringBuilder(numStr.substring(i + 1)).reverse().toString());
+
+        long result = Long.parseLong(numStr.toString());
+        return (result > n && result <= Integer.MAX_VALUE) ? (int) result : -1;
+    }
+}
