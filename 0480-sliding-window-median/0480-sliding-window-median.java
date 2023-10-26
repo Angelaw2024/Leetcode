eafbac98ff3f01d0a59e36873d2d9b19d2ff520d
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-26 15:25:35 -0700

    Time: 106 ms (48.54%), Space: 55.6 MB (37.31%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.java
+++ 0480-sliding-window-median/0480-sliding-window-median.java
@@ -1,96 +1,73 @@
-class Solution {
-    // PriorityQueue<Integer> left;
-    // PriorityQueue<Integer> right;
-    // public double[] medianSlidingWindow1(int[] nums, int k) {
-    //     if (nums == null || nums.length == 0 || nums.length < k) {
-    //         return new double[]{};
-    //     }
-    //     int n = nums.length;
-    //     double[] res = new double[n - k + 1];
-
-    //     left = new PriorityQueue<>(Collections.reverseOrder());
-    //     right = new PriorityQueue<>();
-
-    //     for (int i = 0; i <= n; i++) {
-    //         if (i >= k) {
-    //             res[i - k] = getMedian();
-    //             remove(nums[i - k]);
-    //         }
-
-    //         if (i < n) {
-    //             add(nums[i]);
-    //         }
-    //     }
-    //     return res;
-    // }
-
-    // // maintain left size = right size + 1 if odd k
-    // private double getMedian() {
-    //     if (left.isEmpty() && right.isEmpty()) {
-    //         return 0;
-    //     } else if (left.size() == right.size()) {
-    //         return ((double) left.peek() + (double) right.peek()) / 2.0;
-    //     } else {
-    //         return left.peek();
-    //     }
-    // }
-
-    // private void add(int val) {
-    //     double median = getMedian();
-    //     if (val <= median) {
-    //         left.offer(val);
-    //     } else {
-    //         right.offer(val);
-    //     }
-    //     balance();
-    // }
-
-    // private void remove(int val) {
-    //     double median = getMedian();
-    //     if (val <= median) {
-    //         left.remove(val);
-    //     } else {
-    //         right.remove(val);
-    //     }
-    //     balance();
-    // }
-
-    // private void balance() {
-    //     if (right.size() > left.size()) {
-    //         left.offer(right.poll());
-    //     } else if (left.size() > right.size() + 1) {
-    //         right.offer(left.poll());
-    //     }
-    // }
-
+class Solution { 
     public double[] medianSlidingWindow(int[] nums, int k) {
-        Comparator<Integer> comparator = (a, b) -> nums[a] != nums[b] ? Integer.compare(nums[a], nums[b]) : a - b;
-        TreeSet<Integer> left = new TreeSet<>(comparator.reversed());
-        TreeSet<Integer> right = new TreeSet<>(comparator);
-        
-        Supplier<Double> median = (k % 2 == 0) ?
-            () -> ((double) nums[left.first()] + nums[right.first()]) / 2 :
-            () -> (double) nums[right.first()];
-        
-        // balance lefts size and rights size (if not equal then right will be larger by one)
-        Runnable balance = () -> { while (left.size() > right.size()) right.add(left.pollFirst()); };
-        
-        double[] result = new double[nums.length - k + 1];
-        
-        for (int i = 0; i < k; i++) left.add(i);
-        balance.run(); result[0] = median.get();
-        
-        for (int i = k, r = 1; i < nums.length; i++, r++) {
-            // remove tail of window from either left or right
-            if(!left.remove(i - k)) right.remove(i - k);
+        double[] res = new double[nums.length - k + 1];
+        int count = 0;
 
-            // add next num, this will always increase left size
-            right.add(i); left.add(right.pollFirst());
+        Comparator<Integer> comparator = (a, b) -> nums[a] == nums[b] ? a - b : Integer.compare(nums[a], nums[b]);
+        TreeSet<Integer> left = new TreeSet<>(comparator);
+        TreeSet<Integer> right = new TreeSet<>(comparator);
+
+        for (int i = 0; i < nums.length; i++) {
+            right.add(i);
+            balance(left, right);
             
-            // rebalance left and right, then get median from them
-            balance.run(); result[r] = median.get();
+            if (i >= k - 1) {
+                if (i >= k) {
+                    if (!left.remove(i - k)) {
+                        right.remove(i - k);
+                    }
+                    balance(left, right);
+                }
+                if (k % 2 == 0) {
+                    res[count++] = ((double) nums[left.last()] + nums[right.first()]) / 2;
+                } else {
+                    res[count++] = (double) nums[left.last()];
+                }
+            }
         }
-        
-        return result;
+        return res;
+    }
+    
+    // Method to balance left and right TreeSets
+    public void balance(TreeSet<Integer> left, TreeSet<Integer> right ) {
+        while (left.size() <= right.size()) {
+            left.add(right.pollFirst());
+        }
+        while (left.size() > right.size() + 1) {
+            right.add(left.pollLast());
+        }
+    }
+}
+
+class Solution_480 {
+
+        public double[] medianSlidingWindow(int[] nums, int k) {
+        double[] res = new double[nums.length-k+1];
+        int count = 0;
+        TreeSet<Integer> left = new TreeSet<>((a,b) -> nums[a] == nums[b] ? a-b : Integer.compare(nums[a], nums[b]));
+        TreeSet<Integer> right = new TreeSet<>((a,b) -> nums[a] == nums[b] ? a-b : Integer.compare(nums[a], nums[b]));
+        for(int i=0;i<nums.length;++i) {
+            right.add(i);
+            left.add(right.pollFirst());
+            if (left.size()-right.size()>1)
+                right.add(left.pollLast());
+
+            if (left.size()+right.size() > k) {
+                if (!left.remove(i-k))
+                    right.remove(i-k);
+                if (left.size() < right.size())
+                    left.add(right.pollFirst());
+                if (left.size()-right.size()>1)
+                    right.add(left.pollLast());
+            }
+            if (left.size()+right.size() == k) {
+                if (k%2 == 0) {
+                    res[count++] = ((double)nums[left.last()] + nums[right.first()])/2;
+                }
+                else
+                    res[count++] = nums[left.last()];
+            }
+        }
+        return res;
     }
 }
