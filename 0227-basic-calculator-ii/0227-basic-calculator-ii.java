b96ff3d6fb437ff4f322aeaf3d909529f7b2a48f
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-10-29 16:15:12 -0700

    Time: 11 ms (94.34%), Space: 43.8 MB (82.41%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -2,30 +2,30 @@ class Solution {
     public int calculate(String s) {
         int res = 0;
         int num = 0;
-
         int cur = 0;
         char prevOp = '+';
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (Character.isDigit(c)) {
                 num = num * 10 + c - '0';
-            } 
+            }
             if (c == '+' || c == '-' || c == '*' || c == '/' || i == s.length() - 1) {
-               if (prevOp == '+'){
+                if (prevOp == '+') {
                     cur += num;
-               } else if (prevOp == '-') {
-                   cur -= num;
-               } else if (prevOp == '*') {
-                   cur *= num;
-               } else if (prevOp == '/') {
-                   cur /= num;
-               }
-               if (c == '+' || c == '-' || i == s.length() - 1) {
-                   res += cur;
-                   cur = 0;
-               }
-               prevOp = c;
-               num = 0;
+                } else if (prevOp == '-') {
+                    cur -= num;
+                } else if (prevOp == '*') {
+                    cur *= num;
+                } else if (prevOp == '/') {
+                    cur /= num;
+                }
+
+                if (c == '+' || c == '-' || i == s.length() - 1) {
+                    res += cur;
+                    cur = 0;
+                }
+                prevOp = c;
+                num = 0;
             }
         }
         return res;
