509bfc0a0cdab2577607606b97b14073e4a78e1a
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-11-04 19:27:14 -0700

    Time: 9 ms (87.84%), Space: 50.3 MB (71.37%) - LeetHub

--- 0267-palindrome-permutation-ii/0267-palindrome-permutation-ii.java
+++ 0267-palindrome-permutation-ii/0267-palindrome-permutation-ii.java
@@ -7,36 +7,37 @@ class Solution {
 
         // Check if the string can form a 
         int oddCount = 0;
-        String mid = "";
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 26; i++) {
             if (dict[i] % 2 == 1) {
                 oddCount++;
-                mid += (char)(i + 'a');
+                sb.append((char)(i + 'a'));
+                dict[i]--;
             }
-            dict[i] /= 2; // Only need half of each char for forming half palindrome
         }
 
         // More than one char with odd count cannot form a palindrome
         if (oddCount > 1) return new ArrayList<String>();
 
         List<String> res = new ArrayList<>();
-        backtrack(dict, res, new StringBuilder(), s.length() / 2, mid);
+        backtrack(dict, res, sb, s.length());
         return res;
     }
 
-    public void backtrack(int[] dict, List<String> res, StringBuilder half, int length, String mid) {
-        if (half.length() == length) {
-            res.add(half.toString() + mid + half.reverse().toString());
-            half.reverse(); // Need to reverse back to keep the original order for backtracking
+    public void backtrack(int[] dict, List<String> res, StringBuilder sb, int length) {
+        if (sb.length() == length) {
+            res.add(sb.toString());
             return;
         }
         for (int i = 0; i < 26; i++) {
             if (dict[i] > 0) {
-                dict[i]--;
-                half.append((char)(i + 'a'));
-                backtrack(dict, res, half, length, mid);
-                half.deleteCharAt(half.length() - 1);
-                dict[i]++;
+                dict[i] -= 2;
+                sb.append((char)(i + 'a'));
+                sb.insert(0, (char)(i + 'a'));
+                backtrack(dict, res, sb, length);
+                sb.deleteCharAt(sb.length() - 1);
+                sb.deleteCharAt(0);
+                dict[i] +=2;
             } 
         }
     }
