c6e0be6ad4f40b0bdab395f45882ed6677906022
Angela <64678314+Angela-HappyCoding@users.noreply.github.com>
2023-11-11 13:11:06 -0800

    Time: 114 ms (35.76%), Space: 55.4 MB (75.17%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.java
+++ 0480-sliding-window-median/0480-sliding-window-median.java
@@ -1,4 +1,4 @@
-class Solution { 
+class Solution {
     public double[] medianSlidingWindow(int[] nums, int k) {
         double[] res = new double[nums.length - k + 1];
         int count = 0;
@@ -27,47 +27,13 @@ class Solution {
         }
         return res;
     }
-    
-    // Method to balance left and right TreeSets
-    public void balance(TreeSet<Integer> left, TreeSet<Integer> right ) {
-        while (left.size() <= right.size()) {
-            left.add(right.pollFirst());
-        }
-        while (left.size() > right.size() + 1) {
-            right.add(left.pollLast());
-        }
-    }
-}
-
-class Solution_480 {
-
-        public double[] medianSlidingWindow(int[] nums, int k) {
-        double[] res = new double[nums.length-k+1];
-        int count = 0;
-        TreeSet<Integer> left = new TreeSet<>((a,b) -> nums[a] == nums[b] ? a-b : Integer.compare(nums[a], nums[b]));
-        TreeSet<Integer> right = new TreeSet<>((a,b) -> nums[a] == nums[b] ? a-b : Integer.compare(nums[a], nums[b]));
-        for(int i=0;i<nums.length;++i) {
-            right.add(i);
-            left.add(right.pollFirst());
-            if (left.size()-right.size()>1)
+// Method to balance left and right TreeSets
+        public void balance(TreeSet<Integer> left, TreeSet<Integer> right ) {
+            while (left.size() <= right.size()) {
+                left.add(right.pollFirst());
+            }
+            while (left.size() > right.size() + 1) {
                 right.add(left.pollLast());
-
-            if (left.size()+right.size() > k) {
-                if (!left.remove(i-k))
-                    right.remove(i-k);
-                if (left.size() < right.size())
-                    left.add(right.pollFirst());
-                if (left.size()-right.size()>1)
-                    right.add(left.pollLast());
-            }
-            if (left.size()+right.size() == k) {
-                if (k%2 == 0) {
-                    res[count++] = ((double)nums[left.last()] + nums[right.first()])/2;
-                }
-                else
-                    res[count++] = nums[left.last()];
             }
         }
-        return res;
-    }
 }
